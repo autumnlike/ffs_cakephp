4de742cb19eb86259116e1117823787c7ac9be1f
autumnlike <akiyoshi.satoshi@lancers.co.jp>
2022-11-05 14:23:51 +0900

    refec: インポート処理をサービス層へ

--- src/Command/ImportByEthosCsvCommand.php
+++ src/Command/ImportByEthosCsvCommand.php
@@ -6,8 +6,7 @@ namespace App\Command;
 use Cake\Command\Command;
 use Cake\Console\Arguments;
 use Cake\Console\ConsoleIo;
-use Cake\ORM\TableRegistry;
-use Cake\I18n\FrozenTime;
+use App\Service\Ethos\ImportByCsvService;
 use SplFileObject;
 
 /**
@@ -20,66 +19,10 @@ class ImportByEthosCsvCommand extends Command
     {
         // 仮
         $filename = TMP . 'ETHOS.csv';
-        $file = new SplFileObject($filename);
-
-        // 事前チェック
-        // ETHOSからダウンロード時に指定できるため
-        $encode = exec("nkf -g {$filename}");
-        if ($encode !== 'UTF-8') {
-            $io->abort('ファイルがUTF-8ではありません: ' . $encode);
-        }
-        
-        // @see https://www.php.net/manual/ja/class.splfileobject.php#splfileobject.constants
-        $file->setFlags(
-            SplFileObject::READ_CSV |
-            SplFileObject::READ_AHEAD |
-            SplFileObject::SKIP_EMPTY |
-            SplFileObject::DROP_NEW_LINE
-        );
-
-        $membersTable = TableRegistry::getTableLocator()->get('Members');
-        $memberFfsDiagnosesTable = TableRegistry::getTableLocator()->get('MemberFfsDiagnoses');
-        $memberStressesTable = TableRegistry::getTableLocator()->get('MemberStresses');
-        foreach ($file as $num => $row) {
-            if ($num === 0) {
-                // 先頭行はヘッダ
-                continue;
-            }
-            // TODO 同じ人は重複させない
-            $member = $membersTable->newEmptyEntity();
-            $member->key = $row[1];
-            $member->email = $row[2];
-            $member->name = $row[3] . $row[4];
-
-            $memberFfsDiagnos = $memberFfsDiagnosesTable->newEmptyEntity();
-            $memberFfsDiagnos->a = $row[7];
-            $memberFfsDiagnos->b = $row[8];
-            $memberFfsDiagnos->c = $row[9];
-            $memberFfsDiagnos->d = $row[10];
-            $memberFfsDiagnos->e = $row[11];
-            $memberFfsDiagnos->four_type = $row[13];
-            $memberFfsDiagnos->ninety_one_type = $row[14];
-
-            // ストレス診断履歴
-            $memberStresses = [];
-            foreach ([18, 20, 22, 24, 26, 28] as $key) {
-                if ((bool)$row[$key] === FALSE) {
-                    // 直近から入るのでこれ以降入ることは無い
-                    break;
-                }
-
-                // TODO 同じストレス診断は重複させない
-                $memberStress = $memberStressesTable->newEmptyEntity();
-                $memberStress->point = $row[$key];
-                $memberStress->diagnostic_at = new FrozenTime($row[$key+1]);
-                $memberStresses[] = $memberStress;
-            }
-            $connection = $membersTable->getConnection();
-            $member->member_ffs_diagnosis = $memberFfsDiagnos;
-            $member->member_stresses = $memberStresses;
-            // TODO 値チェック
-            // TODO 失敗時の出力
-            $membersTable->save($member);
+        try {
+            ImportByCsvService::import($filename);
+        } catch (Exception $e) {
+            $io->abort($e->getMessage());
         }
     }
 }
